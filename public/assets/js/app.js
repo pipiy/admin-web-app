4a2f73221a7c5accc6bf02ee90ad4060a2d8c405
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-03-31 12:00:40 +0300

    fixed bug

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,7 +6,7 @@ app.config(function($stateProvider, $urlRouterProvider){
 	$stateProvider
 
 	.state('/', {
-    url:          '/'
+    url:          '/',
 		templateUrl:  'login.html',
 		controller: 	'loginCtrl'
 	})
